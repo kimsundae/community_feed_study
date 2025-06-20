2a5f56976671973b7bb498d44bafaa3d07d725c8
sksdu_2573 <naa159@naver.com>
2025-06-20 16:46:20 +0900

    1차 - 커뮤니티 피드

---
+++ src/main/java/org/fastcampus/post/domain/Post.java
@@ -0,0 +1,18 @@
+package org.fastcampus.post.domain;
+import org.fastcampus.user.domain.User;
+
+public class Post{
+
+    private final Long id;
+    private final User author;
+    private final PostContent content;
+
+    public Post(Long id, User author, PostContent content){
+        if(author == null){
+            throw new IllegalArgumentException();
+        }
+        this.id = id;
+        this.author = author;
+        this.content = content;
+    }
+}
