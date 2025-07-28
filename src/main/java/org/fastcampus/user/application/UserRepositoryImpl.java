23a92e113805ccc1b459ef8d2433569ed04a4899
sksdu_2573 <naa159@naver.com>
2025-07-28 23:44:12 +0900

    패키지명 변경

---
+++ src/main/java/org/fastcampus/user/application/UserRepositoryImpl.java
@@ -0,0 +1,19 @@
+package org.fastcampus.user.application;
+
+import org.fastcampus.user.application.interfaces.UserRepository;
+import org.fastcampus.user.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.empty();
+    }
+}
