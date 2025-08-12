1f6e63cd3d59edb2259b26b45fced5d7f1c024e9
sksdu_2573 <naa159@naver.com>
2025-08-12 23:28:31 +0900

    04 인수테스트 적용

--- src/test/java/org/fastcampus/acceptance/FeedAcceptanceTest.java
+++ src/test/java/org/fastcampus/acceptance/FeedAcceptanceTest.java
@@ -1,11 +1,17 @@
 package org.fastcampus.acceptance;
 
-import org.fastcampus.acceptance.steps.FeedAcceptanceSteps;
+import static org.fastcampus.acceptance.steps.FeedAcceptanceSteps.requestCreatePost;
+import static org.fastcampus.acceptance.steps.FeedAcceptanceSteps.requestFeed;
+
 import org.fastcampus.acceptance.utils.AcceptanceTestTemplate;
 import org.fastcampus.post.application.dto.CreatePostRequestDto;
 import org.fastcampus.post.domain.content.PostPublicationState;
+import org.fastcampus.user.ui.dto.GetPostContentResponseDto;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class FeedAcceptanceTest extends AcceptanceTestTemplate {
 
@@ -23,10 +29,17 @@ public class FeedAcceptanceTest extends AcceptanceTestTemplate {
     * User 1 Get Post 1 From Feed
     * */
     @Test
-    void givenUserHasFollowerWhenFollowingUserCreatePost_thenFollowerCanGetPostFromFeed(){
+    void givenUserHasFollowerAndCreatePostWhenFollowerUserRequestFeed_thenFollowerCanGetPostFromFeed(){
         // given
         CreatePostRequestDto dto = new CreatePostRequestDto(2L, "user 11 can get this post",
                 PostPublicationState.PUBLIC);
-        Long createPostId = FeedAcceptanceSteps.requestCreatePost(dto);
+        Long createPostId = requestCreatePost(dto);
+
+        //when, 팔로워 피드를 요청
+        List<GetPostContentResponseDto> result = requestFeed(1L);
+
+        // then
+        assertEquals(1, result.size());
+        assertEquals(createPostId, result.get(0).getId());
     }
 }
