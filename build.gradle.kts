e5edb8a01dfd1a087912ebdd07fa04abde4f7730
sksdu_2573 <naa159@naver.com>
2025-09-01 23:44:55 +0900

    02. admin 템플릿 적용 및 기본 설정_2

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     runtimeOnly("com.mysql:mysql-connector-j")
 
     implementation("org.projectlombok:lombok")
---
+++ src/main/java/org/fastcampus/admin/ui/AdminController.java
@@ -0,0 +1,15 @@
+package org.fastcampus.admin.ui;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    @GetMapping("/index")
+
+}
